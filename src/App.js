998ae5ea06da2fe5bb07bb110638a8d877db78a8
ckscode <cksiddhaarthan@gmail.com>
2022-04-22 16:25:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,34 +2,58 @@ import React from "react"
 import {useState} from "react"
 import axios from "axios"
 import "./style.css"
-const Toggle=()=>{
-    const [photo,setPhoto]=useState("")
-    const [clientID,setClientId]=useState("6KEric3FgZRQKtt29TgdyYJ64fXmQG-sX_oRZGi5Ymw")
-    const [result,setResult]=useState([])
-   const handleChange=(e)=>{
-      setPhoto(e.target.value)
-      
+const App=()=>{
+    const [location,setLocation]=useState('')
+    const [data,setData]=useState([])
+    const url=`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=7045ff42f23d49e7601563eb17b37bfd&units=metric`
+    
+  
+    const dataChange=(event)=>{
+        if(event.key==="Enter"){
+        axios.get(url).then((res)=>{setData(res.data)
+        console.log(res.data)})
+        setLocation('')
+        
+        }
     }
-    const handleClick=()=>{
-      
-       axios.get(`https://api.unsplash.com/search/photos?page=3&query=${photo}&client_id=${clientID}`).then((res)=>setResult(res.data.results))
-    }
-    return(
-        <div className="container text-center"><div className="py-4"><h1>Unsplash App</h1>
-        <input id="inp" className="form-control mt-3 w-50 mx-auto" type="text" name="photo" onChange={handleChange}></input>
-        <button className="btn btn-primary m-3 text-light" type="submit" onClick={handleClick}>Get!</button></div>
-        <div className="container-fluid">
-        <div className="d-flex justify-content-center flex-row flex-wrap">
-            
-        {result.map((item)=>
-        <div className="col-12 col-sm-5 col-lg-4 p-2 ">
-            <div className="w-100"><img className="h-100 w-100 shadow " src={item.urls.small} alt="" ></img></div> </div>) }
-           
-            </div>
-           </div>
-       </div>
-    )
+  
 
+return(<div className="app container-fluid text-light">
+ <div className="search text-center" >
+<input type="text"  className="px-2 py-1" name="city" value={location} onChange={(e)=>setLocation(e.target.value)} placeholder="location" 
+onKeyPress={dataChange}></input>
+</div>
+<div className="top pt-5">
+<div className="location text-center  mt-4">
+    <h3 >{data.name}</h3>
+</div>
+<div className="temp " >
+    {data.main?<h1 className=" my-1 text-center bold">{data.main.temp.toFixed()}&#176;C</h1>:null}
+</div>
+<div className="description">
+    {data.weather?<h2 className="slightbold">{data.weather[0].main}<br></br>
+    <img src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}></img></h2>:null}
+    
+</div></div>
+{data.main != undefined?
+    <div className="bottom py-2 text-center rounded-3 d-flex flex-wrap  justify-content-evenly mx-auto">
+    <div className="feels  ">
+        {data.main?<h3 className="slightbold">{data.main.feels_like.toFixed()}&#176;C</h3>:null}
+          <p >Feels Like</p>
+    </div>
+    <div className="humidity   ">
+        {data.main?<h3 className="slightbold">{data.main.humidity.toFixed()}%</h3>:null}
+        <p>Humidity</p>
+    </div>
+    <div className="wind ">
+        {data.wind? <h3 className="slightbold">{data.wind.speed.toFixed()}MPS</h3>:null}
+       
+        <p>Wind</p>
+    </div>
+</div>:null}
+
+</div>)
+ 
 }
 
-export default Toggle
+export default App
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import Toggle from "./App"
-ReactDOM.render(<div><Toggle></Toggle></div>,document.getElementById("root"))
+import App from "./App"
+ReactDOM.render(<div><App></App></div>,document.getElementById("root"))
